35d4598a302e7a864f6fe992cede72c36c461dc5
Noa <noa.gratacos@orange.fr>
2023-12-13 10:42:57 +0100

    Delete data var

--- src/components/Charts/BarChart/index.jsx
+++ src/components/Charts/BarChart/index.jsx
@@ -1,44 +1,6 @@
 import './BarChart.css';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  {
-    "day": "2020-07-01",
-    "kilogram": 80,
-    "calories": 240
-  },
-  {
-    "day": "2020-07-02",
-    "kilogram": 80,
-    "calories": 220
-  },
-  {
-    "day": "2020-07-03",
-    "kilogram": 81,
-    "calories": 280
-  },
-  {
-    "day": "2020-07-04",
-    "kilogram": 81,
-    "calories": 290
-  },
-  {
-    "day": "2020-07-05",
-    "kilogram": 80,
-    "calories": 160
-  },
-  {
-    "day": "2020-07-06",
-    "kilogram": 78,
-    "calories": 162
-  },
-  {
-    "day": "2020-07-07",
-    "kilogram": 84,
-    "calories": 390
-  },
-];
-
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
     return (
